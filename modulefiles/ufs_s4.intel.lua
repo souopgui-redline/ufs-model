a95ca3f72daa8b6074273c59496c4dfc161e9a4b
Innocent Souopgui <isouopgui@redlineperf.com>
2024-01-26 19:42:16 +0000

    Update modulefile to spack-stack 1.6.0 for s4

--- modulefiles/ufs_s4.intel.lua
+++ modulefiles/ufs_s4.intel.lua
@@ -2,7 +2,8 @@ help([[
 loads UFS Model prerequisites for S4/Intel
 ]])
 
-prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 load(pathJoin("stack-intel", stack_intel_ver))
@@ -10,7 +11,13 @@ load(pathJoin("stack-intel", stack_intel_ver))
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.0"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
+cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+load(pathJoin("cmake", cmake_ver))
+
 load("ufs_common")
+
+nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
+load(pathJoin("nccmp", nccmp_ver))
 
 setenv("CC", "mpiicc")
 setenv("CXX", "mpiicpc")
@@ -18,3 +25,4 @@ setenv("FC", "mpiifort")
 setenv("CMAKE_Platform", "s4.intel")
 
 whatis("Description: UFS build environment")
+
